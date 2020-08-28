4933776a9daa90b04714244a1345c703906a97bf
jawchewa <jawchewas@gmail.com>
2020-08-27 21:46:25 -0600

    Adding missing files

---
+++ precache-manifest.ccba09f28ca0070c8ab1e5c43df3850b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16fd249f1377161e953d4c37b1ba5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b31e749d5cf9b0d1e676",
+    "url": "/static/css/main.a6080415.chunk.css"
+  },
+  {
+    "revision": "9f4e93e21e6140a52e41",
+    "url": "/static/js/2.60820c23.chunk.js"
+  },
+  {
+    "revision": "adcea22b9efcfbe444252441857232d8",
+    "url": "/static/js/2.60820c23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b31e749d5cf9b0d1e676",
+    "url": "/static/js/main.35f03fa9.chunk.js"
+  },
+  {
+    "revision": "a2dc253bb217aa4c0495",
+    "url": "/static/js/runtime-main.b18aff93.js"
+  }
+]);

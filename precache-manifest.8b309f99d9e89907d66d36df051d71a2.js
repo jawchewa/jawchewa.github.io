415ab5e8d5e401a2313e3a031c93b99ad572c62e
jawchewa <jawchewas@gmail.com>
2020-08-31 13:36:09 -0600

    fixing lfs?

--- precache-manifest.8b309f99d9e89907d66d36df051d71a2.js
+++ precache-manifest.8b309f99d9e89907d66d36df051d71a2.js
@@ -1,3 +1,26 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:466b1f2e92beeae87a1e9d7f4a559ca850935a20c8e9399dc73e965f7354401f
-size 653
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cde57c2f8d6304569ca427531edc827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9001f3d5740659412c0",
+    "url": "/static/css/main.908b7ca6.chunk.css"
+  },
+  {
+    "revision": "094ff8f373a57c66775f",
+    "url": "/static/js/2.8ac95fca.chunk.js"
+  },
+  {
+    "revision": "adcea22b9efcfbe444252441857232d8",
+    "url": "/static/js/2.8ac95fca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9001f3d5740659412c0",
+    "url": "/static/js/main.70e07a65.chunk.js"
+  },
+  {
+    "revision": "a2dc253bb217aa4c0495",
+    "url": "/static/js/runtime-main.b18aff93.js"
+  }
+]);

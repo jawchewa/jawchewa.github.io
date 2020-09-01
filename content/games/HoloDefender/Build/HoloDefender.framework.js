584f2de2502df31d3d4831e7b7b21c616dab004a
jawchewa <jawchewas@gmail.com>
2020-08-31 18:38:59 -0600

    Removing files to clean repo

--- content/games/HoloDefender/Build/HoloDefender.framework.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:89f1cbcf1079d662866757e075255d601fa72d18b8121678e1a4f4c239c0aa04
-size 465869
--- service-worker.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e19873512208b0cd626a30492a1c499a2e354c6d8ebded9c7118cbc13bc8ee64
-size 1183

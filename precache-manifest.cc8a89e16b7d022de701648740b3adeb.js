23d1a3150ebe8b04ea3b7e803e6efa836a166e66
jawchewa <jawchewas@gmail.com>
2020-09-10 17:25:36 -0600

    Adding new stuff

---
+++ precache-manifest.cc8a89e16b7d022de701648740b3adeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3654e2df535ab1b871e464d51218b4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26eda46b68cea8376a6f",
+    "url": "/static/css/main.17601a98.chunk.css"
+  },
+  {
+    "revision": "4c8000c6314521ac740e",
+    "url": "/static/js/2.bfe6c098.chunk.js"
+  },
+  {
+    "revision": "391bd1161dbcd8c291e67f6f483476eb",
+    "url": "/static/js/2.bfe6c098.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26eda46b68cea8376a6f",
+    "url": "/static/js/main.35b4df98.chunk.js"
+  },
+  {
+    "revision": "a2dc253bb217aa4c0495",
+    "url": "/static/js/runtime-main.b18aff93.js"
+  }
+]);
